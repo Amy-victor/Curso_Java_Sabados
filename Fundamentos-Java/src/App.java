f889266b20ad2e2664db0e5922d3d80810e1d679
amy <victorhugobaiajunqueira@gmail.com>
2024-09-21 16:50:33 -0300

    arquivos java

--- Fundamentos-Java/src/App.java
+++ Fundamentos-Java/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Olá, Mundo!");
+
+        System.out.println("Olá, Turma");
+        int a, b, c;
+        a=10;
+        b=20; 
+        c=a+b;
+        System.out.println("Valor de C:" + c);
+        System.out.println(a+ " " +b+ " " +c);
+
+        c = 2+2;
+        System.out.println("Valor de C:"+c);
     }
 }
---
+++ Fundamentos-Java/src/ExercicioEscreverComentar.java
@@ -0,0 +1,15 @@
+public class ExercicioEscreverComentar {
+    
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo, eu sou Amy!");
+
+    // Exemplo de comentário de uma linha
+       
+     /*
+    Exemplo de
+    comentário de
+    múltiplas linhas
+    */
+    
+    }
+}
