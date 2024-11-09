027f46a274d3cbfd692e5fb68deaf51548fa20f3
Amy <victorhugobaiajunqueira@gmail.com>
2024-11-09 13:32:02 -0300

    teste

--- Fundamentos-Java/src/App.java
+++ Fundamentos-Java/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Olá, Mundo!");
+        System.out.println("Olá! Mundo!");
 
         System.out.println("Olá, Turma");
         int a, b, c;
